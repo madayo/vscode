b1426b0ef5e80aa80ff14b6d9a4f133bf07f50b2
madayo <madayodama@gmail.com>
2017-12-18 14:29:29 +0900

    拡張機能の自動インストール

--- extensions_install.sh
+++ extensions_install.sh
@@ -1,5 +1,8 @@
 #! /bin/sh -e
 for ext in `cat extensions_list.txt`; do
   array=( `echo "$ext" | sed -E 's/^(.*)\.(.*)@(.*)$/\1 \2 \3/g'` )
-  echo "http://${array[0]}.gallery.vsassets.io/_apis/public/gallery/publisher/${array[0]}/extension/${array[1]}/${array[2]}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"
+  curl -O "https://${array[0]}.gallery.vsassets.io/_apis/public/gallery/publisher/${array[0]}/extension/${array[1]}/${array[2]}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"
+  mv Microsoft.VisualStudio.Services.VSIXPackage Microsoft.VisualStudio.Services.VSIXPackage.vsix
+  code --install-extension Microsoft.VisualStudio.Services.VSIXPackage.vsix
 done
+rm Microsoft.VisualStudio.Services.VSIXPackage.vsix
