af742532d54f04e01899cf0dea44e416bc8223b4
madayo <madayodama@gmail.com>
2019-04-23 15:58:31 +0900

    更新

--- extensions_install.sh
+++ extensions_install.sh
@@ -1,7 +1,8 @@
 #! /bin/sh -e
 for ext in `cat extensions_list.txt`; do
+  printf "\e[36;40;1m${ext}\e[m\n"
   array=( `echo "$ext" | sed -E 's/^(.*)\.(.*)@(.*)$/\1 \2 \3/g'` )
-  curl -O "https://${array[0]}.gallery.vsassets.io/_apis/public/gallery/publisher/${array[0]}/extension/${array[1]}/${array[2]}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage"
+  curl -O "https://${array[0]}.gallery.vsassets.io/_apis/public/gallery/publisher/${array[0]}/extension/${array[1]}/${array[2]}/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage" -Ss
   mv Microsoft.VisualStudio.Services.VSIXPackage Microsoft.VisualStudio.Services.VSIXPackage.vsix
   code --install-extension Microsoft.VisualStudio.Services.VSIXPackage.vsix
 done
